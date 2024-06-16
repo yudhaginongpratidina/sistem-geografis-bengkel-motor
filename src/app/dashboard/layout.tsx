d23287a4600eab1c5ad1217179a869b9dde5eae2
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2024-06-16 16:24:13 +0700

    modify navbar dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,14 @@
 import FooterView from "@/views/footer"
+import Link from "next/link"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <nav className="w-full py-2 bg-white shadow-md drop-shadow-md">
                 <div className="container flex justify-between items-center">
-                    <h1>DASHBOARD</h1>
+                    <Link href={"/dashboard"}>
+                        <h1 className="text-2xl font-bold">DASHBOARD</h1>
+                    </Link>
                     <button className="py-2 px-4 bg-rose-500 text-white font-semibold">Logout</button>
                 </div>
             </nav>
