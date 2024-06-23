84b15ecf9700806a3c2354bfcb68db267bd13954
yudhaginongpratidina <dev.yudhaginongpratidina@gmail.com>
2024-06-23 19:03:23 +0700

    modify prisma-global

--- src/libs/prisma-global.ts
+++ src/libs/prisma-global.ts
@@ -1,15 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient | undefined;
+
+try {
+  prisma = new PrismaClient();
+} catch (error) {
+  console.error('Failed to initialize Prisma Client:', error);
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
